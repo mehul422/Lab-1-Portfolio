a95006079682acb529b0760a07cd68ac20208df3
Mehul Verma <meverma@ucsd.edu>
2025-01-23 11:33:59 -0800

    step 3 part 1 done

--- global.js
+++ global.js
@@ -4,10 +4,26 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-navLinks = $$("nav a");
+// navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+// let currentLink = navLinks.find(
+    // (a) => a.host === location.host && a.pathname === location.pathname
+//   );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: 'index.html', title: 'Home' },
+    { url: 'projects/index.html', title: 'Projects' },
+    { url: 'contact/index.html', title: 'Contact' },
+    { url: 'cv/resume/index.html', title: 'Resume/CV' },
+  ]; 
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
