ddbb476bc8133d4a32eaba2148dae1aac13ccae4
Mehul Verma <meverma@ucsd.edu>
2025-02-01 13:40:58 -0800

    step 1 done

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,4 @@
 import { fetchJSON, renderProjects } from '../global.js';
-const projects = await fetchJSON('./lib/projects.json');
+const projects = await fetchJSON('https://mehul422.github.io/Lab-1-Portfolio/lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
