5f509e7bc901321cdb61dcc0ab4c0b9d54add591
Mehul Verma <meverma@ucsd.edu>
2025-02-01 13:34:54 -0800

    step 1 done

--- projects/projects.js
+++ projects/projects.js
@@ -1,14 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
-
-// Fetch the project data
 const projects = await fetchJSON('/lib/projects.json');
-
-// Get the container where projects will be displayed
 const projectsContainer = document.querySelector('.projects');
 
-// Render all projects to the container
-projects.forEach(project => renderProjects(project, projectsContainer, 'h2'));
-
-// Get the project count element and set its text content
-const projectCountElement = document.getElementById('project-count');
-projectCountElement.textContent = projects.length; // Set the count to the number of projects
+renderProjects(projects, projectsContainer, 'h2');
