cc7b3cffe3031314680e6f47e56aa1ca1580becc
Mehul Verma <meverma@ucsd.edu>
2025-02-01 15:23:51 -0800

    all done

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';  // Im
 // Fetch project data
 const projects = await fetchJSON('https://mehul422.github.io/Lab-1-Portfolio/lib/projects.json');
 
-// Filter the first 3 projects
-const latestProjects = projects.slice(0, 3);
+// Determine which page we're on
+const isHomePage = document.body.classList.contains('home');
+const isProjectsPage = document.body.classList.contains('projects');
 
 // Select the container where the projects will be displayed
 const projectsContainer = document.querySelector('.projects');
 
-// Render the filtered projects
-renderProjects(latestProjects, projectsContainer, 'h2');
+// Render the appropriate number of projects based on the page
+if (isHomePage) {
+  // On the home page, render only the first 3 projects
+  const latestProjects = projects.slice(0, 3);
+  renderProjects(latestProjects, projectsContainer, 'h2');
+} else if (isProjectsPage) {
+  // On the projects page, render all projects
+  renderProjects(projects, projectsContainer, 'h2');
+}
 
 const githubData = await fetchGitHubData('mehul422');
 
@@ -25,4 +33,4 @@ if (profileStats) {
             <dt>Following:</dt><dd>${githubData.following}</dd>
           </dl>
       `;
-  }
+}
