aa17440c04d632e3105de1240fd952da2a27c754
Mehul Verma <meverma@ucsd.edu>
2025-02-01 13:43:56 -0800

    step 1 done

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,17 @@
 import { fetchJSON, renderProjects } from '../global.js';
+
+// Fetch the project data
 const projects = await fetchJSON('https://mehul422.github.io/Lab-1-Portfolio/lib/projects.json');
+
+// Select the projects-title element
+const projectsTitle = document.querySelector('.projects-title');
+
+// Calculate the number of projects and update the text of the h1 tag
+const projectCount = projects.length;
+projectsTitle.textContent = `${projectCount} Projects`; // Example: "12 Projects"
+
+// Select the container to render projects into
 const projectsContainer = document.querySelector('.projects');
+
+// Render the projects
 renderProjects(projects, projectsContainer, 'h2');
