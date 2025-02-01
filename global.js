92766d27b7cdaad2fba9e8507d115d855846dc9e
Mehul Verma <meverma@ucsd.edu>
2025-02-01 14:02:47 -0800

    step 3 done

--- global.js
+++ global.js
@@ -128,4 +128,8 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
   });
 }
 
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${mehul422}`);
+}
+
   
