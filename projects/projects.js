f5692f9cc78082502ddd39a0e889de03ffa177ea
Mehul Verma <meverma@ucsd.edu>
2025-02-06 23:56:09 -0800

    step 1.4 done

--- projects/projects.js
+++ projects/projects.js
@@ -28,11 +28,32 @@ if (projects.length === 0) {
     }, 500); // Wait for 500ms before rendering the projects
 }
 
+let colors = ['gold', 'purple'];
+
 let arc = d3.arc().innerRadius(0).outerRadius(50)({
     startAngle: 0,
     endAngle: 2 * Math.PI,
   });
-  
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
+let data = [1, 2];
 
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg').append('path').attr('d', arc).attr('fill', colors[idx]);
+  })
