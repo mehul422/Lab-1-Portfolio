ff20e585feb9a559995e25f4dea3f5a1c621f57b
Mehul Verma <meverma@ucsd.edu>
2025-02-07 00:34:07 -0800

    step 2.2 done

--- projects/projects.js
+++ projects/projects.js
@@ -34,10 +34,17 @@ let colors = d3.scaleOrdinal(d3.schemeTableau10); // Using the schemeTableau10 c
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 // Pie chart data
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+  ];
 
 // Use d3.pie() to generate start and end angles
-let sliceGenerator = d3.pie(); // This returns a function that calculates start/end angles for slices
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data); // arcData now contains objects with startAngle, endAngle, and value
 
 // Generate the arcs for each slice
@@ -51,4 +58,10 @@ arcs.forEach((arc, idx) => {
     .attr('fill', colors(idx)); // Use colors function to get the color for each slice
 });
 
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+})
 
